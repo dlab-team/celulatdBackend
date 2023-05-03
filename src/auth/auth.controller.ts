6ac3d03766e96032bcd4232bfa1aa1bd8f2416c1
susana ingrid arce silva <ingridarce4@gmail.com>
2023-05-03 07:13:10 -0400

    auth role, guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body,  Headers, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto, } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
+import { ValidRoles } from './interfaces/valid-roles';
+import { Auth } from './decorators/auth.decorators';
 @Controller('user')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
@@ -21,9 +27,10 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
+    //decorators parametros
     @GetUser() user: User,
     @GetUser('email') userEmail: string,
-    
+    @Headers() headers: IncomingHttpHeaders,
     ) {
 
     return {
@@ -31,8 +38,34 @@ export class AuthController {
       message:'Hola mundo',
       user,
       userEmail,
-     
+      headers
      }
     }
 
+    // @SetMetadata('roles', ['admin','super-user'])
+
+  @Get('private2')
+  @RoleProtected( ValidRoles.user, ValidRoles.admin )
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+
+    return {
+      ok: true,
+      user
+    }
+  }
+
+  @Get('private3')
+  @Auth( ValidRoles.admin )
+  privateRoute3(
+    @GetUser() user: User
+  ) {
+
+    return {
+      ok: true,
+      user
+    }
+  }
 }
